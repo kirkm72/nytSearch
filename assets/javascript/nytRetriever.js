45ee728b558a43fac2ef2fef1f22601ca9745409
cogitor <52011619+cogitor525@users.noreply.github.com>
2019-08-27 17:08:20 -0500

    fixed bugs w/ search button clicks that wouldn't recognize new inputs after first search

--- assets/javascript/nytRetriever.js
+++ assets/javascript/nytRetriever.js
@@ -1,50 +1,52 @@
-let topic='';
-let key='olLxyaAGoNDfbLbvCY3tB9AmFzj2c75i';
-let start_year;
-let end_year;
-let articleCount;
-
-let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+key+"&q=";
+const key='olLxyaAGoNDfbLbvCY3tB9AmFzj2c75i';
+const queryUrlDefault = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+key+"&q=";
 // let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=olLxyaAGoNDfbLbvCY3tB9AmFzj2c75i";
 // let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=olLxyaAGoNDfbLbvCY3tB9AmFzj2c75i&&begin_date=20120101&end_date=20121231";
 // let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=olLxyaAGoNDfbLbvCY3tB9AmFzj2c75i&&begin_date=20120101&end_date=20121231";
-$("#search-button").on("click", function(event){
+$(document.body).on("click", "#search-button", function(event){
     event.preventDefault();
-    topic = $("#search-term").val();
-    queryURL += topic;
-    start_year = $("#start-year").val();
-    end_year = $("#end-year").val();
-    articleCount = $(".number-select").val();
+    let queryUrl = queryUrlDefault;
+    const topic = $("#search-term").val();
+    queryUrl += topic;
+    const start_year = $("#start-year").val();
+    const end_year = $("#end-year").val();
+    const articleCount = $(".number-select").val();
 
     console.log(start_year);
     console.log(end_year);
 
     if(start_year != ''){
-      queryURL +="&begin_date="+start_year+"0101";
+      queryUrl +="&begin_date="+start_year+"0101";
     }
     if(end_year != ''){
-      queryURL +="&end_date="+end_year+"1231";
+      queryUrl +="&end_date="+end_year+"1231";
     }
-    console.log(queryURL);
+    console.log(queryUrl);
     $.ajax({
-        url: queryURL,
+        url: queryUrl,
         method: "GET"
       })
       .then(function(response){
+        $(".tmpResults").empty();
         for(let i=0; i<articleCount; i++){   
-          let headline=response.response.docs[i].headline.main;
-          let authors=response.response.docs[i].byline.original;
-          
-          const newDiv=$("<div class='tmpResults'>");
-          newDiv.append(i+1)
-          newDiv.append(headline)
-          newDiv.append(authors)
-          $(".panel").append(newDiv);
+          const headline=response.response.docs[i].headline.main;
+          const authors=response.response.docs[i].byline.original;
+          const link=response.response.docs[i].web_url;
 
-      }
+          const newDiv=$("<div class='tmpResults'>");
+          const newLink=$("<a>");
+          newLink.attr("href", link);
+          newLink.attr("target", "_blank");
+          newLink.text(headline);
+
+          newDiv.append((i+1) + ": ");
+          newDiv.append(newLink);
+          newDiv.append(" - " + authors);
+          $(".panel").append(newDiv);
+        }
       })
       .catch(function(error){
-          alert("error");
+        alert("error");
       });
       // alert("end");
 });
@@ -52,5 +54,8 @@ $("#search-button").on("click", function(event){
 $("#clear-all").on("click", function(event){
   event.preventDefault();
   $(".tmpResults").empty();
-  
-})
+  $("#search-term").val("");
+  $("#start-year").val("");
+  $("#end-year").val("");
+  $(".number-select").val("5");
+});
